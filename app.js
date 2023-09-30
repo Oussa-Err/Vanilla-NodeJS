75ec7aea9f3fc224fcec60cd1ecbba62434928a0
Oussa-Err <dexterlecter0@gmail.com>
2023-09-30 22:32:18 +0100

    url parse query param and used it for routing

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const { response } = require("express")
 const fs = require("fs")
 const http = require('http')
+const url = require("url")
 
 
 const html = fs.readFileSync("./Template/index.html", "utf-8")
 let products = JSON.parse(fs.readFileSync("./Data/products.json", "utf-8"))
 let productListHtml = fs.readFileSync("./Template/product-list.html", "utf-8")
+let productDetailHtml = fs.readFileSync("./Template/product-details.html", 'utf-8')
 
-let productHtmlArray = products.map((prod) => {
-    let output = productListHtml.replace("{{%MODELIMAGE%}}", prod.productImage)
-    output = output.replace('{{%PHONENAME%}}', prod.name)
-    output = output.replace('{{%MODELNAME%}}', prod.modeName)
-    output = output.replace('{{%MODELNO%}}', prod.modelNumber)
-    output = output.replace('{{%MODELSIZE%}}', prod.size)
-    output = output.replace('{{%CAMERA%}}', prod.camera)
-    output = output.replace('{{%PRICE%}}', prod.price)
-    output = output.replace('{{%COLOR%}}', prod.color)
+function replaceHtml(template, product){
+    let output = template.replace("{{%MODELIMAGE%}}", product.productImage)
+    output = output.replace('{{%PHONENAME%}}', product.name)
+    output = output.replace('{{%MODELNAME%}}', product.modeName)
+    output = output.replace('{{%MODELNO%}}', product.modelNumber)
+    output = output.replace('{{%MODELSIZE%}}', product.size)
+    output = output.replace('{{%CAMERA%}}', product.camera)
+    output = output.replace('{{%PRICE%}}', product.price)
+    output = output.replace('{{%COLOR%}}', product.color)
+    output = output.replace('{{%ID%}}', product.id)
+    output = output.replace('{{%ROM%}}', product.ROM)
+    output = output.replace('{{%DESC%}}', product.Description)
 
     return output
-})
+}
 
 const server = http.createServer((request, response) => {
-    const path = request.url
+    let { query, pathname: path } = url.parse(request.url, true)
+
     if (path === '/' || path.toLocaleLowerCase() === "/home") {
         response.writeHead(200, {
             'Content-Type': 'text/html',
@@ -44,11 +49,18 @@ const server = http.createServer((request, response) => {
         response.end(html.replace("{{%CONTENT%}}", `you are in the contact page`))
     }
     else if (path.toLocaleLowerCase() === '/products') {
-        let productResponseArray = html.replace('{{%CONTENT%}}', productHtmlArray.join(','))
-        response.writeHead(200, {
-            'Content-Type': 'text/html',
-        })
-        response.end(productResponseArray)
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        if (!query.id) {
+            let productHtmlArray = products.map((prod) => {
+                return replaceHtml(productListHtml, prod)
+            })
+            let productResponseArray = html.replace('{{%CONTENT%}}', productHtmlArray.join(''))
+            response.end(productResponseArray)
+        } else {
+            let prod = products[query.id]
+            let productDetailResponseHtml = replaceHtml(productDetailHtml, prod )
+            response.end(html.replace('{{%CONTENT%}}', productDetailResponseHtml))    
+        }
     }
     else {
         response.writeHead(404,
